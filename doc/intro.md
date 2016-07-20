469e742e552946d3606ac7e2a437382525f78c67
eunmin <telepopsound@gmail.com>
2016-07-21 00:04:04 +0900

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to poeaa
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/poeaa/core_test.clj
@@ -0,0 +1,7 @@
+(ns poeaa.core-test
+  (:require [clojure.test :refer :all]
+            [poeaa.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
